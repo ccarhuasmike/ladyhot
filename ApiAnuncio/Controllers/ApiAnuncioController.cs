b99c9e83c6a2c0a3bdf00c8f3e42691b6624bbe7
ccarhuasmike <ccarhuas.mike@gmail.com>
2019-01-31 11:46:45 -0500

    se agrego un web api

---
+++ ApiAnuncio/Controllers/ApiAnuncioController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLogic;
+using Communities;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiAnuncio.Controllers
+{
+    [RoutePrefix("api/anuncio")]
+    public class ApiAnuncioController : ApiController
+    {
+        [Route("ListarAnuncio"), HttpPost]
+        public ClientResponse sel_cliente(JObject objData)
+        {
+            ClientResponse clientResponse = new ClientResponse();
+            try
+            {
+                clientResponse = new AnuncioLogic().ListarAnuncio();
+            }
+            catch (Exception ex)
+            {
+                clientResponse = Utilidades.ObtenerMensajeErrorWeb(ex);
+            }           
+            return clientResponse;
+        }
+    }
+}
