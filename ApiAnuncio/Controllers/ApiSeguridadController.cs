c3d6f320976077fca340fdbb04e4c8597c38b1cb
ccarhuasmike <ccarhuas.mike@gmail.com>
2019-09-08 02:44:19 -0500

    nuevo cambios

--- ApiAnuncio/Controllers/ApiSeguridadController.cs
+++ ApiAnuncio/Controllers/ApiSeguridadController.cs
@@ -67,6 +67,58 @@ namespace ApiAnuncio.Controllers
         }
 
 
+        [Route("ReestablecerContrasnia"), HttpPost]
+        public ClientResponse ReestablecerContrasnia(Bean_mail beanMail)
+        {
+            ClientResponse clientResponse = new ClientResponse();
+            try
+            {
+                Tbl_parameter_det entidad_det = null;
+                entidad_det = new Tbl_parameter_det()
+                {
+                    paramter_cab = new Tbl_parameter_cab() { skey_cab = "SKEY_MAIL" }
+                };
+                IEnumerable<Tbl_parameter_det> lista = new ParameterLogic().GetParameter_skey(entidad_det);
+
+                Tbl_parameter_det user = lista.ToList().Where(x => x.skey_det.Equals("SKEY_MAIL_DET_USER")).FirstOrDefault();
+                Tbl_parameter_det clave = lista.Where(x => x.skey_det.Equals("SKEY_MAIL_DET_CLAVE")).FirstOrDefault();
+                Tbl_parameter_det smtp = lista.Where(x => x.skey_det.Equals("SKEY_MAIL_DET_SMTP")).FirstOrDefault();
+                Tbl_parameter_det puerto = lista.Where(x => x.skey_det.Equals("SKEY_MAIL_DET_PUERTO")).FirstOrDefault();
+
+                Tbl_parameter_det entidad_det_site_lady_hot = null;
+                entidad_det_site_lady_hot = new Tbl_parameter_det()
+                {
+                    paramter_cab = new Tbl_parameter_cab() { skey_cab = "SKEY_SITE_LADYHOT" }
+                };
+                IEnumerable<Tbl_parameter_det> lista_site_lady_hot = new ParameterLogic().GetParameter_skey(entidad_det_site_lady_hot);
+                Tbl_parameter_det site_lady_hot = lista_site_lady_hot.ToList().Where(x => x.skey_det.Equals("SKEY_SITE_LADYHOT_RUTA")).FirstOrDefault();
+
+                var url = System.Web.Hosting.HostingEnvironment.MapPath("/template/templatereestablecerpassword.html");
+                string mensaje = "";
+                mensaje = Utilidades.LeerTemplateHTML(url);
+                Tbl_usuario entididad_usuario = new UsuarioLogic().getUsuarioPorCorreo(new Tbl_usuario() { tx_email = beanMail.para.First() });
+                mensaje = mensaje.Replace("{email}", beanMail.para.First());
+                mensaje = mensaje.Replace("{url_escogerpassword}", site_lady_hot.tx_descripcion + "seguridad/EscogerPassword/" + entididad_usuario.tx_token);
+
+                mensaje = HttpUtility.HtmlDecode(mensaje);
+                beanMail.puerto = int.Parse(puerto.tx_valor);
+                beanMail.de = user.tx_valor;
+                beanMail.para = beanMail.para;
+                beanMail.clave = clave.tx_valor;
+                beanMail.smtpServer = smtp.tx_valor;
+                beanMail.body = mensaje;
+                beanMail.asunto = beanMail.asunto;
+                clientResponse = Mail.EnvioMailSegundo(beanMail);
+            }
+            catch (Exception ex)
+            {
+                clientResponse = Utilidades.ObtenerMensajeErrorWeb(ex);
+            }
+            return clientResponse;
+        }
+
+
+
         [Route("ActualizarPasswordUsuario"), HttpPost]
         public ClientResponse ActualizarPasswordUsuario(Tbl_usuario usuario)
         {
