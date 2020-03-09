99fc4aa845bbbee7ce96ba40bc7672a64f5c8b9a
ccarhuasmike <ccarhuas.mike@gmail.com>
2020-03-08 19:01:35 -0500

    nuevos

--- WebAnuncio/obj/Release/Package/PackageTmp/js/megamenu.js
+++
@@ -1,9 +0,0 @@
-var dMegaMenu = $(document);
-var miMegaMenu = {};
-miMegaMenu.inicializar = function () {
-    $(document).on("change", ".j_megamenu_provincias", function () {
-        console.log(this.value);
-        location.href=this.value;
-    });
-};
-dMegaMenu.ready(miMegaMenu.inicializar);
